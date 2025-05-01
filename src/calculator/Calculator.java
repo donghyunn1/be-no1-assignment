a35f680d5412036181aac9ad004407dedbca0f81
양동현 <dongdong0516@naver.com>
2025-05-01 14:25:02 +0900

    feat: 연산 진행하기

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -15,5 +15,24 @@ public class Calculator {
 
         System.out.print("사칙연산 기호를 입력하세요: ");
         char operator = sc.nextLine().charAt(0);
+
+        int result = 0;
+        if (operator == '+') {
+            result = firstNum + secondNum;
+        } else if (operator == '-') {
+            result = firstNum - secondNum;
+        } else if (operator == '*') {
+            result = firstNum * secondNum;
+        } else if (operator == '/') {
+            if (secondNum == 0) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            } else {
+                result = firstNum / secondNum;
+            }
+        } else {
+            System.out.println("유효하지 않은 연산자 입니다!");
+        }
+
+        System.out.println("결과: " + result);
     }
 }
