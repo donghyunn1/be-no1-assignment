9f8e5bb40d7f4e97b4abe8093beab28542ff5f39
양동현 <dongdong0516@naver.com>
2025-05-01 14:16:19 +0900

    feat: 양의 정수를 입력받기

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello, Calculator!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int firstNum = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int secondNum = sc.nextInt();
     }
 }
